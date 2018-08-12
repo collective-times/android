b7eb8a0becf9930f55e19c23e0052a888e37323f
moonstruckdrops <moonstruckdrops@gmail.com>
2018-08-12 15:17:52 +0900

    add navigation config for detail

--- App.js
+++ App.js
@@ -5,13 +5,27 @@ import ArticleScreen from './ArticleScreen';
 
 const RootStack = createStackNavigator(
   {
-    Home: ArticleListScreen,
-    Detail: ArticleScreen,
+    Home: { screen: ArticleListScreen },
+    Detail: {
+      screen: ArticleScreen,
+      navigationOptions: ({ navigation} ) => ({
+        title: navigation.state.params.title.slice(0, 17),
+        headerStyle: {
+          backgroundColor: '#c8f277de',
+        },
+        headerTintColor: '#000000',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontFamily: 'Roboto',
+        },
+      }),
+    },
   },
   {
     initialRouteName: 'Home',
   }
 );
+
 
 export default class App extends React.Component {
   render() {
