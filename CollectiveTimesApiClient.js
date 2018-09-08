9f8928418dfca8dc41fbbcb790fd45ce5f3fdb37
moonstruckdrops <moonstruckdrops@gmail.com>
2018-09-08 16:51:33 +0900

    add login api

--- CollectiveTimesApiClient.js
+++ CollectiveTimesApiClient.js
@@ -2,25 +2,39 @@ import axios from 'axios';
 
 export default class CollectiveTimesApiClient {
 
-  static get API_ENDPOINT() { return "https://collective-times-api.herokuapp.com/v1/"; };
+  static get API_ENDPOINT() { return "https://collective-times-api.herokuapp.com/v1"; };
+
+  async login(username, password, callback) {
+    const res = await axios.post(`${CollectiveTimesApiClient.API_ENDPOINT}/login`,
+                                 {
+                                   username: username,
+                                   password: password
+                                 });
+    if (res.status === 201) {
+      callback(res.data);
+    } else {
+      callback([]);
+    }
+  }
 
   async getArticles(page, callback) {
-    const res = await axios.get(`${CollectiveTimesApiClient.API_ENDPOINT}articles?page=${page}`);
-    if (res.status !== 200) {
-      callback([]);
-    } else {
+    const res = await axios.get(`${CollectiveTimesApiClient.API_ENDPOINT}/articles?page=${page}`);
+    if (res.status === 200) {
       callback(res.data.articles);
+    } else {
+      callback([]);
     }
   };
 
-  async saveVisitedArticleBy(articleId) {
-    const res = await axios.post(`${CollectiveTimesApiClient.API_ENDPOINT}histories`, {
-      article_id: articleId
-    });
-    if (res.status !== 200) {
-      console.log('fail');
-    } else {
+  async saveVisitedArticleBy(token, articleId) {
+    const res = await axios.post(`${CollectiveTimesApiClient.API_ENDPOINT}/histories`,
+                                 {
+                                   article_id: articleId
+                                 });
+    if (res.status === 200) {
       console.log('success');
+    } else {
+      console.log('fail');
     }
   };
 }
